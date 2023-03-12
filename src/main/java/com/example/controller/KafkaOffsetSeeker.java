1c77520796c5a1dbc28ab43ef406a60c6de4bab0
kamal <kamaldkannan@gmail.com>
2023-03-12 10:05:01 -0400

    test

--- src/main/java/com/example/controller/KafkaOffsetSeeker.java
+++ src/main/java/com/example/controller/KafkaOffsetSeeker.java
@@ -1,57 +1,91 @@
+import java.util.*;
+import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLException;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.*;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
 @RestController
 @RequestMapping("/api")
-@Api(value = "Kafka Offset Seeker API")
 public class KafkaOffsetSeeker {
 
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.group-id}")
+    private String groupId;
+
+    @Value("${kafka.ssl.truststore-location}")
+    private String truststoreLocation;
+
+    @Value("${kafka.ssl.truststore-password}")
+    private String truststorePassword;
+
     @Autowired
-    private KafkaConsumer<String, String> kafkaConsumer;
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
-    @ApiOperation(value = "Get the offset for a given consumer group and partition.")
-    @GetMapping("/offset/{group}/{topic}/{partition}")
-    public ResponseEntity<Long> getOffset(
-            @PathVariable String group,
-            @PathVariable String topic,
-            @PathVariable int partition
-    ) {
-        TopicPartition topicPartition = new TopicPartition(topic, partition);
+    private KafkaConsumer<String, Object> consumer;
 
+    @PostConstruct
+    private void initConsumer() throws SSLException {
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // SSL configuration
+        props.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, truststoreLocation);
+        props.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, truststorePassword);
+        props.setProperty(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "");
+
+        // Set up deserializer with error handling
+        JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class, false);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        ErrorHandlingDeserializer<Object> errorHandlingDeserializer = new ErrorHandlingDeserializer<>(deserializer);
+
+        consumer = new KafkaConsumer<>(props, new StringDeserializer(), errorHandlingDeserializer);
+    }
+
+    @GetMapping("/offsets")
+    public ResponseEntity<String> seekOffsets(@RequestBody Map<String, Long> partitionOffsets) {
         try {
-            // Assign the topic partition to the consumer
-            kafkaConsumer.assign(Collections.singletonList(topicPartition));
+            String topic = "my-topic";
+            consumer.subscribe(Collections.singletonList(topic));
 
-            // Seek to the beginning of the partition
-            kafkaConsumer.seekToBeginning(Collections.singletonList(topicPartition));
+            // Poll for records to ensure consumer is assigned partitions
+            consumer.poll(Duration.ofSeconds(5));
 
-            // Poll the partition to get the current offset
-            kafkaConsumer.poll(Duration.ofMillis(100));
-            long currentOffset = kafkaConsumer.position(topicPartition);
+            // Seek to specified partition offsets
+            for (Map.Entry<String, Long> entry : partitionOffsets.entrySet()) {
+                String[] parts = entry.getKey().split("-");
+                int partition = Integer.parseInt(parts[1]);
+                long offset = entry.getValue();
+                consumer.seek(new TopicPartition(topic, partition), offset);
+                System.out.printf("Group: %s, Topic: %s, Partition: %d, Offset: %d\n",
+                        groupId, topic, partition, offset);
+            }
 
-            // Seek back to the end of the partition
-            kafkaConsumer.seekToEnd(Collections.singletonList(topicPartition));
-
-            // Poll the partition to get the end offset
-            kafkaConsumer.poll(Duration.ofMillis(100));
-            long endOffset = kafkaConsumer.position(topicPartition);
-
-            // Seek back to the current offset
-            kafkaConsumer.seek(topicPartition, currentOffset);
-
-            return ResponseEntity.ok(endOffset - currentOffset);
+            return ResponseEntity.ok("Offsets seeked successfully.");
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error seeking offsets: " + e.getMessage());
         }
     }
 
-    @ApiOperation(value = "Check if the API is running.")
-    @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("API is running.");
-    }
-
-    @ApiOperation(value = "Get the API version.")
-    @GetMapping("/version")
-    public ResponseEntity<String> getVersion() {
-        return ResponseEntity.ok("1.0.0");
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOffsetSeeker.class, args);
     }
 }
