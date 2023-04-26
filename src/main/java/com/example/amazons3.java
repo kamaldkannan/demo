72f8bec325abf60c610927d09acd433f116a53cf
kamaldkannan <kamaldkannan@gmail.com>
2023-04-26 14:06:58 -0400

    Update amazons3.java

--- src/main/java/com/example/amazons3.java
+++ src/main/java/com/example/amazons3.java
@@ -1,3 +1,69 @@
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.descriptors.FileSystem;
+import org.apache.flink.table.descriptors.OldCsv;
+import org.apache.flink.table.descriptors.Schema;
+import org.apache.flink.table.factories.TableFactoryService;
+import org.apache.flink.table.sources.wmstrategies.AscendingTimestamps;
+import org.apache.flink.table.sources.wmstrategies.BoundedOutOfOrderTimestamps;
+import org.apache.flink.table.sources.wmstrategies.PreserveWatermarks;
+import org.apache.flink.table.sources.wmstrategies.WatermarkStrategy;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.types.Row;
+
+StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+// Specify the S3 file path
+Path s3FilePath = new Path("s3://<bucket>/<path>/<file>.csv");
+
+// Define the CSV table schema
+TableSchema schema = TableSchema.builder()
+  .field("id", DataTypes.STRING())
+  .field("name", DataTypes.STRING())
+  .field("age", DataTypes.INT())
+  .build();
+
+// Define the watermark strategy (if applicable)
+WatermarkStrategy<Row> watermarkStrategy = new BoundedOutOfOrderTimestamps(5000);
+
+// Configure the FileSystem and OldCsv descriptors
+tableEnv.connect(new FileSystem().path(s3FilePath.toString()))
+  .withFormat(new OldCsv()
+    .fieldDelimiter(",")
+    .lineDelimiter("\n")
+    .deriveSchema())
+  .withSchema(new Schema()
+    .field("id", DataTypes.STRING())
+    .field("name", DataTypes.STRING())
+    .field("age", DataTypes.INT()))
+  .createTemporaryTable("csv_table");
+
+// Get the DynamicTableSource from the temporary table
+Table csvTable = tableEnv.from("csv_table");
+DynamicTableSource csvTableSource = TableFactoryService.find(
+  DynamicTableSourceFactory.class,
+  "csv"
+).createDynamicTableSource(csvTable.getSchema());
+
+// Register the DynamicTableSource as a table
+tableEnv.registerTableSource("csv_table", csvTableSource);
+
+// Set the watermark strategy (if applicable)
+if (watermarkStrategy != null) {
+  tableEnv.registerWatermarkStrategy("csv_table", watermarkStrategy);
+}
+
+// Query the CSV table and print the results
+Table resultTable = tableEnv.sqlQuery("SELECT * FROM csv_table");
+tableEnv.toRetractStream(resultTable, Row.class).print();
+
+
 dependencies {
     implementation 'org.apache.flink:flink-table-api-java:1.16.1'
     implementation 'org.apache.flink:flink-table-planner-blink_2.12:1.16.1'
